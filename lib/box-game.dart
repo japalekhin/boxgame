b57c137547f1b3d6e628f877dc187fb1b6d1bcb7
Japa Alekhin Llemos <jap@alekhin.io>
2019-02-24 15:56:53 +0800

    override resize function of game class store the size of a screen in a variable

--- lib/box-game.dart
+++ lib/box-game.dart
@@ -3,11 +3,18 @@ import 'dart:ui';
 import 'package:flame/game.dart';
 
 class BoxGame extends Game {
+  Size screenSize;
+
   void render(Canvas canvas) {
     // TODO: implement render
   }
 
   void update(double t) {
     // TODO: implement update
+  }
+
+  void resize(Size size) {
+    screenSize = size;
+    super.resize(size);
   }
 }

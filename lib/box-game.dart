3d6ce81fcfb50914f9c894968bc3ac35fc05c3ce
Japa Alekhin Llemos <jap@alekhin.io>
2019-02-24 15:10:07 +0800

    added boxgame class

---
+++ lib/box-game.dart
@@ -0,0 +1,13 @@
+import 'dart:ui';
+
+import 'package:flame/game.dart';
+
+class BoxGame extends Game {
+  void render(Canvas canvas) {
+    // TODO: implement render
+  }
+
+  void update(double t) {
+    // TODO: implement update
+  }
+}

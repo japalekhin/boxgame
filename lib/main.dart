9390e4484c996694ae9c4ea594dd85409459f516
Japa Alekhin Llemos <jap@alekhin.io>
2019-02-24 17:45:24 +0800

    registered a tapgesturerecognizer and linked to game handler

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:boxgame/box-game.dart';
 import 'package:flame/util.dart';
+import 'package:flutter/gestures.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter/material.dart';
 
@@ -9,5 +10,8 @@ void main() async {
   await flameUtil.setOrientation(DeviceOrientation.portraitUp);
 
   BoxGame game = BoxGame();
+  TapGestureRecognizer tapper = TapGestureRecognizer();
+  tapper.onTapDown = game.onTapDown;
   runApp(game.widget);
+  flameUtil.addGestureRecognizer(tapper);
 }

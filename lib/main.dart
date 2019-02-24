72f5beb2113cd1dc569e6dade6293bfca7108c63
Japa Alekhin Llemos <jap@alekhin.io>
2019-02-24 15:22:20 +0800

    created boxgame instance run app using widget property of boxgame instance

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:boxgame/box-game.dart';
 import 'package:flame/util.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter/material.dart';
@@ -6,4 +7,7 @@ void main() async {
   Util flameUtil = Util();
   await flameUtil.fullScreen();
   await flameUtil.setOrientation(DeviceOrientation.portraitUp);
+
+  BoxGame game = BoxGame();
+  runApp(game.widget);
 }
